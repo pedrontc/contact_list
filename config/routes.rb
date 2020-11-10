4acd079648c2810a6c35ce4085291a7af07c9ed9
pedrojscript <pedrofigueiredoads@gmail.com>
2020-11-10 13:30:50 -0400

    Criando Model User

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,7 @@ Rails.application.routes.draw do
   get 'sobre', to: 'static_pages#sobre'
   get 'contato', to: 'static_pages#contato'
 
+  resources :contacts
+  resources :users, only: [:new, :create]
+
 end
